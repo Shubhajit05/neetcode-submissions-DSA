7b0da23e3447e5e28f5ad26cd654f51f76d2480e
Shubhajit05 <shubhakun236@gmail.com>
2026-09-06 20:55:01 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            // Linear search for the minimum element
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap
+            int temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+        }
+        return nums;
+    }
+}
